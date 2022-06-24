d1d96c1767525f01fd385c3fc9ef5fd3ea81126b
Elliot Yim <yimsb02@naver.com>
2022-06-24 21:43:51 +0900

    Implement ControllerExceptionHandler to handle improper parameters

---
+++ src/main/java/com/elliot/tinyurl/advice/ControllerExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.elliot.tinyurl.advice;
+
+import com.elliot.tinyurl.errors.ErrorCode;
+import com.elliot.tinyurl.errors.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        StringBuilder errors = new StringBuilder();
+        ex.getBindingResult().getAllErrors()
+                .forEach(e -> errors.append(((FieldError) e).getField() + " : " + e.getDefaultMessage() + "; " ));
+
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_PARAMETER);
+        response.setDetail(errors.toString());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleRequestBodyMissingException(HttpMessageNotReadableException ex) {
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_PARAMETER);
+        response.setDetail(ex.getMessage().split(":")[0]);
+        return ResponseEntity.badRequest().body(response);
+    }
+}
