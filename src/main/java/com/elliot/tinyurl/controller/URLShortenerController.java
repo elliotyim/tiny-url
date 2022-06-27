07f9e483fcbb2301b06ab5c7304a88516633598a
Elliot Yim <yimsb02@naver.com>
2022-06-27 14:36:56 +0900

    Add omitted annotation

--- src/main/java/com/elliot/tinyurl/controller/URLShortenerController.java
+++ src/main/java/com/elliot/tinyurl/controller/URLShortenerController.java
@@ -2,17 +2,17 @@ package com.elliot.tinyurl.controller;
 
 import com.elliot.tinyurl.dto.request.URLRequestDTO;
 import com.elliot.tinyurl.dto.response.URLResponseDTO;
+import com.elliot.tinyurl.model.URL;
+import com.elliot.tinyurl.repository.URLRepository;
 import com.elliot.tinyurl.service.URLService;
 import com.elliot.tinyurl.service.URLShortenerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("shortener")
@@ -22,6 +22,8 @@ public class URLShortenerController {
     private final URLService urlService;
 
     private final URLShortenerService urlShortenerService;
+
+    private final URLRepository urlRepository;
 
     @PostMapping
     public ResponseEntity<URLResponseDTO> shortenUrl(
@@ -33,6 +35,12 @@ public class URLShortenerController {
             urlResponseDTO = urlService.saveUrl(urlRequestDTO);
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(urlResponseDTO);
+    }
+
+    @GetMapping("urls")
+    public ResponseEntity<Object> getUrls() throws Exception {
+        List<URL> urls = urlRepository.findAll();
+        return ResponseEntity.ok(urls);
     }
 
 }
--- src/main/java/com/elliot/tinyurl/model/URL.java
+++ src/main/java/com/elliot/tinyurl/model/URL.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
         })
 @Getter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class URL {
     @Id
     @Column(name = "short_url")
